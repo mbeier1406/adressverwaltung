59cfeab6f9acaa6196071b34a0b5e6f78683d507
Martin Beier <Martin.Beier@gmx.de>
2023-02-12 20:09:00 +0100

    GET-Service über ID und Properties

--- adressverwaltung-jee/src/main/java/com/github/mbeier1406/adressverwaltung/jee/PersonServlet.java
+++ adressverwaltung-jee/src/main/java/com/github/mbeier1406/adressverwaltung/jee/PersonServlet.java
@@ -12,15 +12,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.github.mbeier1406.adressverwaltung.jee.model.Person;
 
 /**
- * Servlet implementation class PersonServlet: CRUD-Implementierung für {@linkplain Person} über http.
+ * CRUD-Implementierung für {@linkplain Person} über http.
  */
 @WebServlet("/PersonServlet")
 public class PersonServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
+	public static final Logger LOGGER = LogManager.getLogger(PersonServlet.class);
 
 	/** Der Datenbank-Service CRUD */
 	@EJB
@@ -33,10 +38,12 @@ public class PersonServlet extends HttpServlet {
         super();
     }
 
+    /** Typ des DB-Services setzen */
+    @Override
     public void init() throws ServletException {
         if ( personService == null ) throw new UnavailableException("personService ist null.");
         personService.setType(Person.class);
-        System.out.println("Initialized!");
+        LOGGER.info("Initialized!");
     }
 
 	/**
@@ -48,12 +55,13 @@ public class PersonServlet extends HttpServlet {
 	 * </p>
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("get: " + request + " / " + response);
+		LOGGER.trace("get: {}", request);
 		final var prop = request.getParameter("prop");
 		final var id = request.getParameter("id");
 		final var val = request.getParameter("val");
-		System.out.println("id: " + id + "; prop: " + prop + "; val:" + val);
+		LOGGER.debug("id: {}" + id + "; prop: " + prop + "; val:" + val);
 		Person p;
 		if ( id != null )
 			p = personService.findById(Long.parseLong(id));
@@ -66,18 +74,18 @@ public class PersonServlet extends HttpServlet {
 		}
 	}
 
-	@Override
-	  protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			System.out.println("put: " + request + " / " + response);
-			final var p = new Person("Michelle", "Wichtig", new Date(), Person.Geschlecht.WEIBLICH, null);
-			System.out.println("p=" + p);
-			personService.persist(p);
-			response.getWriter().append("Served at: ").append(request.getContextPath()).append(p.toString());
-			System.out.println("p=" + p);
-//	      int userId = retrieveUserid(req);
-//	      String body = inputStreamToString(req.getInputStream());
-//	      System.out.println("body: " + body);
-//	      UserDataService.Instance.saveUserById(userId, body);
-	}
+//	@Override
+//	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("put: " + request + " / " + response);
+//		final var p = new Person("Michelle", "Wichtig", new Date(), Person.Geschlecht.WEIBLICH, null);
+//		System.out.println("p=" + p);
+//		personService.persist(p);
+//		response.getWriter().append("Served at: ").append(request.getContextPath()).append(p.toString());
+//		System.out.println("p=" + p);
+////	      int userId = retrieveUserid(req);
+////	      String body = inputStreamToString(req.getInputStream());
+////	      System.out.println("body: " + body);
+////	      UserDataService.Instance.saveUserById(userId, body);
+//	}
 
 }
