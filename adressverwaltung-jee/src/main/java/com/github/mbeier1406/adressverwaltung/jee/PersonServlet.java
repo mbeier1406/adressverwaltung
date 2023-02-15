e6aa85052e371cccaf093654fad0cee3918ed2e2
Martin Beier <Martin.Beier@gmx.de>
2023-02-15 21:55:01 +0100

    Servlet für Speicherung von Personen vorbereitet.

--- adressverwaltung-jee/src/main/java/com/github/mbeier1406/adressverwaltung/jee/PersonServlet.java
+++ adressverwaltung-jee/src/main/java/com/github/mbeier1406/adressverwaltung/jee/PersonServlet.java
@@ -1,5 +1,8 @@
 package com.github.mbeier1406.adressverwaltung.jee;
 
+import static org.apache.logging.log4j.CloseableThreadContext.put;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.CloseableThreadContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -43,7 +47,7 @@ public class PersonServlet extends HttpServlet {
     public void init() throws ServletException {
         if ( personService == null ) throw new UnavailableException("personService ist null.");
         personService.setType(Person.class);
-        LOGGER.info("Initialized!");
+		LOGGER.info("Server: {}", getServletContext().getServerInfo());
     }
 
 	/**
@@ -57,35 +61,59 @@ public class PersonServlet extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		LOGGER.trace("get: {}", request);
-		final var prop = request.getParameter("prop");
-		final var id = request.getParameter("id");
-		final var val = request.getParameter("val");
-		LOGGER.debug("id: {}" + id + "; prop: " + prop + "; val:" + val);
-		Person p;
-		if ( id != null )
-			p = personService.findById(Long.parseLong(id));
-		else
-			p = personService.findByProperty(prop, val);
-		System.out.println("p=" + p);
-		response.setContentType("text/html");
-		try ( PrintWriter pw = response.getWriter(); ) {
+		try ( CloseableThreadContext.Instance ctx = put("request", request.toString()).put("Info", getServletInfo());
+				PrintWriter pw = response.getWriter(); ) {
+			final var id = request.getParameter("id");
+			final var prop = request.getParameter("prop");
+			final var val = request.getParameter("val");
+			LOGGER.debug("id: {}" + id + "; prop: " + prop + "; val:" + val);
+			Person p;
+			if ( id != null )
+				p = personService.findById(Long.parseLong(id));
+			else
+				p = personService.findByProperty(prop, val);
+			System.out.println("p=" + p);
+			response.setContentType("text/html");
 			pw.println(p.toString());
+		}
+		catch ( Exception e ) {
+			LOGGER.error("request={}", request, e);
 		}
 	}
 
-//	@Override
-//	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		System.out.println("put: " + request + " / " + response);
-//		final var p = new Person("Michelle", "Wichtig", new Date(), Person.Geschlecht.WEIBLICH, null);
-//		System.out.println("p=" + p);
-//		personService.persist(p);
-//		response.getWriter().append("Served at: ").append(request.getContextPath()).append(p.toString());
-//		System.out.println("p=" + p);
-////	      int userId = retrieveUserid(req);
-////	      String body = inputStreamToString(req.getInputStream());
-////	      System.out.println("body: " + body);
-////	      UserDataService.Instance.saveUserById(userId, body);
-//	}
+	/**
+	 * $ wget --quiet --server-response --output-document=- --method=PUT --body-data "ccc^Mxxx" http://localhost:8080/adressverwaltung/PersonServlet
+	 * 
+	 * @see HttpServlet#doPut(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try ( CloseableThreadContext.Instance ctx = put("request", request.toString()).put("Info", getServletInfo());
+				BufferedReader reader = request.getReader();
+				PrintWriter pw = response.getWriter() ) {
+			for ( String line=""; line != null; line = reader.readLine() )
+				LOGGER.info("line={}", line);
+//			final var p = new Person("Michelle", "Wichtig", new Date(), Person.Geschlecht.WEIBLICH, null);
+//			System.out.println("p=" + p);
+//			personService.persist(p);
+//			response.getWriter().append("Served at: ").append(request.getContextPath()).append(p.toString());
+//			System.out.println("p=" + p);
+	//	      int userId = retrieveUserid(req);
+	//	      String body = inputStreamToString(req.getInputStream());
+	//	      System.out.println("body: " + body);
+	//	      UserDataService.Instance.saveUserById(userId, body);
+			response.setContentType("text/html");
+			pw.println("Gespeichert.");
+		}
+		catch ( Exception e ) {
+			LOGGER.error("request={}", request, e);
+		}
+	}
+
+	/**	{@inheritDoc} */
+	@Override
+	public String getServletInfo() {
+		return "Adressverwaltung, 2023, mdd";
+	}
 
 }
